3ad308bfce0193e0c7746b24ea62ff7c1be15126
Chitrank Sharma <Chitrank.Sharma@igglobal.com>
2019-02-25 11:24:38 +0530

    Updates

---
+++ precache-manifest.c96daae1ae5723b1bc235efffcf4623c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "de5e1f29d74b52d2d08b",
+    "url": "/weather-app/static/js/main.de5e1f29.chunk.js"
+  },
+  {
+    "revision": "eeea146f0cc8abcce0c2",
+    "url": "/weather-app/static/js/1.eeea146f.chunk.js"
+  },
+  {
+    "revision": "bf993efba9d952ad06898f0f57ab11c5",
+    "url": "/weather-app/index.html"
+  }
+];
