a8256a920ba57ce7aaa3e5dfdf45c497f977aec1
Chitrank Sharma <Chitrank.Sharma@igglobal.com>
2019-02-25 16:50:50 +0530

    Testing fix

--- src/component/WebApp.tsx
+++ src/component/WebApp.tsx
@@ -71,7 +71,7 @@ export class WebApp extends React.Component<IWebAppProps, IWebAppStates>{
     }
     renderRedirect(){
         if (!localStorage.getItem('userName') || localStorage.getItem('userName')!.length == 0) {
-          return <Redirect from={process.env.PUBLIC_URL + '/'} to={'/login'} />
+          return <Redirect to={process.env.PUBLIC_URL +'/login'} />
         }
     }
 
